fc9ffe9876d125b45db4d1126f63705d1c633da1
Josue07Villegas <pepitovillegas07@hotmail.com>
2026-06-04 16:55:30 -0600

    añadiendo libreria de Division

---
+++ division.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void division(){
+int num1,num2;
+printf("Dame el valor de el primer numero");
+scanf("%d",&num1);
+printf("dame el valor de el segundo numero");
+scanf("%d",&num2);
+printf("el resultado de la division de %d  / %d = %d",num1,num2,(num1/num2));
+ }
