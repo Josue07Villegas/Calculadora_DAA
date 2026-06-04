f5495e845c9afecda35bd15e27a0e914c47c4cd7
Josue07Villegas <pepitovillegas07@hotmail.com>
2026-06-04 16:53:43 -0600

    agregando la libreria de multiplicacion

---
+++ multiplicacion.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ void multiplicacion(){
+int num1,num2;
+printf("Dame el valor de el primer numero");
+scanf("%d",&num1);
+printf("dame el valor de el segundo numero");
+scanf("%d",&num2);
+printf("el resultado de la multiplicacion de %d + %d = %d",num1,num2,(num1*num2));
+ }
