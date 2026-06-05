1bc6d7092960d4fa2be74d6e6eee14f9a41106e7
Josue07Villegas <pepitovillegas07@hotmail.com>
2026-06-04 19:53:22 -0600

    fix arreglar signos en el print de resultado

--- resta.h
+++ resta.h
@@ -5,5 +5,5 @@ printf("Dame el valor de el primer numero");
 scanf("%d",&num1);
 printf("dame el valor de el segundo numero");
 scanf("%d",&num2);
-printf("el resultado de la resta de %d + %d = %d",num1,num2,(num1-num2));
+printf("el resultado de la resta de %d - %d = %d",num1,num2,(num1-num2));
  }
