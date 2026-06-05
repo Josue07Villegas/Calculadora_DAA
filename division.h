c212ee934869af803887a5c605c05661b2dc4f88
Josue07Villegas <pepitovillegas07@hotmail.com>
2026-06-04 20:12:56 -0600

    mejorando la division

--- division.h
+++ division.h
@@ -5,5 +5,9 @@ printf("Dame el valor de el primer numero");
 scanf("%d",&num1);
 printf("dame el valor de el segundo numero");
 scanf("%d",&num2);
+if (num2==0){
+printf("Error:no Se puede dividir entre cero.\n");
+}else{
 printf("el resultado de la division de %d  / %d = %d",num1,num2,(num1/num2));
- }
+} 
+}
